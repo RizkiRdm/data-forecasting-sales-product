a22464ec05e00a9fe3ee3535a0c245404ffa77ef
RizkiRdm <rizkiromdhoni28@gmail.com>
2025-01-04 20:42:56 +0700

    feat: running process, update: column name

--- pipeline/preprocess.py
+++ pipeline/preprocess.py
@@ -24,11 +24,11 @@ def aggregate_data_monthly(sales):
 
     sales['date'] = pd.to_datetime(sales['date'], format="%d.%m.%Y")
     sales['month'] = sales['date'].dt.to_period("M")
-    monthly_sales = sales.groupby(['month', 'shop_id', 'items_id']).agg({
+    monthly_sales = sales.groupby(['month', 'shop_id', 'item_id']).agg({
         'item_cnt_day': 'sum',
         'item_price': 'mean'
     }).reset_index()
-    monthly_sales.rename(columns={'items_cnt_day' : 'item_cnt_month'}, inplace=True)
+    monthly_sales.rename(columns={'item_cnt_day' : 'item_cnt_month'}, inplace=True)
     
     return monthly_sales
 
@@ -37,3 +37,15 @@ def save_cleaned_data(monthly_sales, output_path):
 
     monthly_sales.to_csv(output_path, index=False)
     print(f"cleaned data saved to {output_path}")
+    
+if __name__ == "__main__":
+    # path I/O files
+    sales_path = "data/sales_train.csv"
+    items_path = "data/items.csv"
+    output_path = "data/cleaned_sales.csv"
+
+    # run process
+    sales, items = load_data(sales_path, items_path)
+    sales = clean_data(sales)
+    monthly_sales = aggregate_data_monthly(sales)
+    save_cleaned_data(monthly_sales, output_path)
